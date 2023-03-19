a5cd887771d2032e245b5159c7bdc07d75937d65
Vinicius C M <vinicius.c.madeira@gmail.com>
2023-03-19 10:56:41 +0000

    Adding functionalities from cy.request()

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -261,5 +261,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
       })
 
+      it('faz uma requisição HTTP', function(){
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response){
+                const { status, statusText, body } = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+            })
+      })
+
   })
   
